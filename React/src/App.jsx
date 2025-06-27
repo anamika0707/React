ff82194a5e543e30063ad96ab552af224d66787d
anamika0707 <141743181+anamika0707@users.noreply.github.com>
2025-06-27 11:39:22 +0000

    React router initial commit

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -6,28 +6,74 @@ import './App.css'
 
 
 // step :1 create context
-const UserContext=createContext();
+// const UserContext=createContext();
 
 //step2: wrap all the child inside a provider
 //step3:pass value
 //step4:consumer ke andar jake consume ker lo
 
-function App() {
+// function App() {
  
-   const [user,setUser]=useState({name:"Anamika"});
+//    const [user,setUser]=useState({name:"Anamika"});
 
 
-  return (
+//   return (
 
    
-    <>
-    <UserContext.Provider value={user}>
-      <ChildA/>
-    </UserContext.Provider>
+//     <>
+//     <UserContext.Provider value={user}>
+//       <ChildA/>
+//     </UserContext.Provider>
       
-    </>
+//     </>
+//   )
+// }
+
+// export default App
+// export {UserContext};
+
+
+//Routing Starts from here for routing install react-router-dom using command npm i react-router-dom
+import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
+
+const router=createBrowserRouter(
+  [  //this array contains all the routes and these routes are inside the curly braces
+    {
+      path:"/",
+      element:<div>
+        <Navbar/>
+        <Home/>
+      </div> //ye batata hai ki is path pe konsa page render kerna hai
+    },
+    { 
+      path:"/about",
+      element:
+      <div>
+        <Navbar/>
+        <About/>
+      </div>
+     },
+    
+     { 
+      path:"/dashboard",
+      element:
+      <div>
+        <Navbar/>
+        <Dashboard/>
+      </div>
+     }
+  ]
+);
+function App(){ 
+  return (
+    <div>
+       <RouterProvider router={router}/> { /* //ye basically page ko batata hai ki ye sab routes hai in sab ko render kerna hai} */}
+    </div>
   )
 }
 
-export default App
-export {UserContext};
+export default App;
---
+++ React/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Home from './Home'
+import Dashboar from './Dashboard'
+import About from './About'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+const Navbar=()=>{
+    return (
+        <div>
+            <ul>
+                {/* //hume anchor tag ki jagah link tag ka use isliye kiya hai kyuki anchor tag pura page load kerta hai joki hum nhi chahte */}
+
+                {/* // hum link tag ka use na kerke navlink ka use isliye ker rhe hai kyuki hum isme styling aur easily ker skte hai due presence of active class in it */}
+                <li><NavLink to="/"className={({isActive})=>isActive?"active-link":""}>Home</NavLink></li>
+                <li><NavLink to="/about" className={({isActive})=>isActive?"active-link":""}>About</NavLink></li>
+                <li><NavLink to="/dashboard" className={({isActive})=>isActive?"active-link":""}>Dashboard</NavLink></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
