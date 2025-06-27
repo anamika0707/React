3f53de1ea729621e1da7b5ed1a16628011a38bb3
anamika0707 <141743181+anamika0707@users.noreply.github.com>
2025-06-27 17:11:42 +0000

    usenavigate done

--- React/src/components/Home.jsx
+++ React/src/components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'; //ye hume help kerta hai ki hum maan lo koi button pe click ker rhe hai to wo dusri page par navigate kara dega
 
 const Home =()=>{
+    const navigate=useNavigate();
+    function handleClick(){
+        navigate('/about'); 
+    }
     return (
         <div>
             Home Page
+            {/* <button onClick={handleClick}>Move to About Page</button> //is button ko click kerke about page pe navigate ho jayenge */}
         </div>
     )
 }
